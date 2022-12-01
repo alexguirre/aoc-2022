94f9963fb909b57893ccc832355d0ed65768c3d0
alexguirre <alexguirre.aa@gmail.com>
2022-12-01 10:09:27 +0100

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,35 @@
+use std::cmp::max;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("data/input1");
+    let res = input
+        .lines()
+        .map(|l| if l.len() > 0 { Some(l.parse::<u32>().unwrap()) } else { None })
+        .fold((0u32, 0u32, [0u32; 3]), |mut acc, n| {
+            // acc.0 = current group sum
+            // acc.1 = max sum (part 1)
+            // acc.2 = stack of top 3 max sums (part 2)
+            if let Some(num) = n {
+                acc.0 += num;
+            } else {
+                acc.1 = max(acc.0, acc.1);
+
+                if acc.1 > acc.2[0] { // try push new max sum into top 3 stack
+                    if acc.2[0] > acc.2[1] {
+                        if acc.2[1] > acc.2[2] {
+                            acc.2[2] = acc.2[1];
+                        }
+                        acc.2[1] = acc.2[0];
+                    }
+                    acc.2[0] = acc.1;
+                }
+
+                acc.0 = 0; // reset sum
+            }
+            acc
+        });
+    let part1 = res.1;
+    let part2 = res.2.iter().copied().reduce(|acc, n| acc + n).unwrap();
+    println!("[Part 1] Result is {part1}");
+    println!("[Part 2] Result is {part2} ({:?})", res.2);
 }
