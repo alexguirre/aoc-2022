041edf2a8e7a95721e42173f10b79d2eeffdb7e7
alexguirre <alexguirre.aa@gmail.com>
2022-12-02 15:00:42 +0100

    Day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+
+    const WIN_LUT: [u8; 3]  = [2, 0, 1]; // Rock defeats Scissors, Paper defeats Rock, Scissors defeats Paper
+    const LOSS_LUT: [u8; 3] = [1, 2, 0];
+
+    let input = include_str!("data/input2");
+    let res1 = input
+        .lines()
+        .map(|l| {
+            let opponent = l.as_bytes()[0] - 'A' as u8;
+            let player = l.as_bytes()[2] - 'X' as u8;
+            let score = player + 1 + if WIN_LUT[player as usize] == opponent { 6 } else if player == opponent { 3 } else { 0 };
+            score as u64
+        })
+        .sum::<u64>();
+    println!("[Part 1] Result is {res1}");
+
+    let res2 = input
+        .lines()
+        .map(|l| {
+            let opponent = l.as_bytes()[0] - 'A' as u8;
+            let outcome = l.as_bytes()[2] - 'X' as u8;
+            let score = outcome * 3 + match outcome {
+                0 /* lose */ => WIN_LUT[opponent as usize] + 1,
+                1 /* draw */ => opponent + 1,
+                2 /* win  */ => LOSS_LUT[opponent as usize] + 1,
+                _ => panic!("unexpected outcome")
+            };
+            score as u64
+        })
+        .sum::<u64>();
+    println!("[Part 2] Result is {res2}");
 }
