5e0947340ab3032418797ec90f2c5d2044eead12
alexguirre <alexguirre.aa@gmail.com>
2022-12-06 09:21:35 +0100

    Day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("data/input6");
+
+    for (part, &marker_length) in [4usize, 14].iter().enumerate() {
+        let (start_index, _) = input.as_bytes()
+            .windows(marker_length)
+            .enumerate()
+            .find(|(_, w)| w.iter().all(|&n| w.iter().filter(|&&m| m == n).count() == 1))
+            .unwrap();
+        let res = start_index + marker_length;
+        println!("[Part {}] Result is {res}", part + 1);
+    }
 }
